d1ced7b2a20e0628d2d438a474c57d84989a320f
MinGyoungBo <mingyoungbo@nexell.co.kr>
2018-09-18 19:38:17 -0700

    Introduce convergence_svma board
    This patch introduces convergence_svma board.
    
    The specification of convergence_svma board is like below.
    - memory: 2GByte(512MByte x 4)
    - LCD: 1920x720
    - EMMC booting
    - SVM camera support
    - PMIC: NXE2000
    
    Change-Id: I844239de1c7261650e0f73aa80c95b15a5e54aa3
    Signed-off-by: MinGyoungBo <mingyoungbo@nexell.co.kr>

---
+++ BoardConfig.mk
@@ -0,0 +1,96 @@
+#
+# Copyright (C) 2015 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a9
+
+TARGET_BOARD_PLATFORM := s5p4418
+TARGET_BOOTLOADER_BOARD_NAME := con_svma
+TARGET_BOARD_INFO_FILE := device/nexell/con_svma/board-info.txt
+
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
+TARGET_NO_BOOTLOADER := false
+TARGET_NO_KERNEL := false
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_RECOVERY := false
+
+# recovery
+TARGET_RELEASETOOLS_EXTENSIONS := device/nexell/con_svma
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_nexell
+TARGET_RECOVERY_FSTAB := device/nexell/con_svma/recovery.fstab
+
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_USES_ALSA_AUDIO := false
+
+BOARD_EGL_CFG := device/nexell/con_svma/egl.cfg
+USE_OPENGL_RENDERER := true
+TARGET_USES_ION := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+
+# see surfaceflinger
+MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
+
+# hwcomposer
+BOARD_USES_NX_HWCOMPOSER := true
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_PIC := true
+    endif
+  endif
+endif
+
+BOARD_CHARGER_ENABLE_SUSPEND := false
+
+# ffmpeg libraries
+EN_FFMPEG_EXTRACTOR := false
+EN_FFMPEG_AUDIO_DEC := false
+
+# sepolicy
+BOARD_SEPOLICY_DIRS += \
+	device/nexell/con_svma/sepolicy
+
+# filesystems
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_CACHEIMAGE_PARTITION_SIZE := 536870912
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 4914675712
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+# boot image layout
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_BOOTLOADER_IS_2ND := true
+
+TARGET_USES_AOSP := true
+
+USE_CLANG_PLATFORM_BUILD := true
+
+# certificate
+PRODUCT_DEFAULT_DEV_CERTIFICATE := device/nexell/con_svma/signing_keys/release
+
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
+
---
+++ device.mk
@@ -0,0 +1,166 @@
+#
+# Copyright (C) 2015 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_SHIPPING_API_LEVEL := 25
+
+# Camera App
+PRODUCT_PACKAGES += \
+	Camera
+
+PRODUCT_COPY_FILES += \
+	device/nexell/con_svma/init.con_svma.rc:root/init.con_svma.rc \
+	device/nexell/con_svma/init.con_svma.usb.rc:root/init.con_svma.usb.rc \
+	device/nexell/con_svma/fstab.con_svma:root/fstab.con_svma \
+	device/nexell/con_svma/ueventd.con_svma.rc:root/ueventd.con_svma.rc \
+	device/nexell/con_svma/init.recovery.con_svma.rc:root/init.recovery.con_svma.rc \
+    device/nexell/con_svma/busybox:system/bin/busybox \
+    device/nexell/con_svma/hwreg_cmd:system/bin/hwreg_cmd \
+    device/nexell/con_svma/memtester:system/bin/memtester \
+    device/nexell/con_svma/memtester:root/memtester
+
+PRODUCT_COPY_FILES += \
+    device/nexell/con_svma/nx_3d_avm/nx_3d_avm:system/bin/nx_3d_avm/nx_3d_avm \
+    device/nexell/con_svma/nx_3d_avm/3DS/gtr3ds_complexed/GTR.3ds:system/bin/nx_3d_avm/3DS/gtr3ds_complexed/GTR.3ds \
+    device/nexell/con_svma/nx_3d_avm/3DS/gtr3ds_complexed/body.bmp:system/bin//nx_3d_avm/3DS/gtr3ds_complexed/body.bmp \
+    device/nexell/con_svma/nx_3d_avm/3DS/gtr3ds/GTR.3ds:system/bin//nx_3d_avm/3DS/gtr3ds/GTR.3ds \
+    device/nexell/con_svma/nx_3d_avm/3DS/gtr3ds/body.bmp:system/bin//nx_3d_avm/3DS/gtr3ds/body.bmp \
+    device/nexell/con_svma/nx_3d_avm/3DS/spot_light_256x256.BMP:system/bin//nx_3d_avm/3DS/spot_light_256x256.BMP \
+    device/nexell/con_svma/nx_3d_avm/avm/input_image/avm_full_data.yuv:system/bin//nx_3d_avm/avm/input_image/avm_full_data.yuv \
+    device/nexell/con_svma/nx_3d_avm/avm/avm_info_3dview.data:system/bin//nx_3d_avm/avm/avm_info_3dview.data \
+    device/nexell/con_svma/nx_3d_avm/avm/avm_info_topview.data:system/bin//nx_3d_avm/avm/avm_info_topview.data
+
+
+PRODUCT_COPY_FILES += \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
+# audio
+
+PRODUCT_COPY_FILES += \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	device/nexell/con_svma/media_codecs.xml:system/etc/media_codecs.xml \
+	device/nexell/con_svma/media_profiles.xml:system/etc/media_profiles.xml \
+	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml
+
+# input
+PRODUCT_COPY_FILES += \
+	device/nexell/con_svma/gpio_keys.kl:system/usr/keylayout/gpio_keys.kl \
+	device/nexell/con_svma/gpio_keys.kcm:system/usr/keychars/gpio_keys.kcm
+
+# hardware features
+PRODUCT_COPY_FILES += \
+	device/nexell/con_svma/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+	frameworks/native/data/etc/android.hardware.faketouch.xml:system/etc/permissions/android.hardware.faketouch.xml
+
+# Recovery
+PRODUCT_PACKAGES += \
+	librecovery_updater_nexell
+
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_CONFIG += mdpi xlarge large
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+PRODUCT_AAPT_PREBUILT_DPI := hdpi mdpi ldpi
+PRODUCT_CHARACTERISTICS := tablet
+
+# OpenGL ES API version: 2.0
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.opengles.version=131072
+
+# density
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=160
+
+# libion needed by gralloc, ogl
+PRODUCT_PACKAGES += libion iontest
+
+PRODUCT_PACKAGES += libdrm
+
+# HAL
+PRODUCT_PACKAGES += \
+	gralloc.con_svma \
+	libGLES_mali \
+	hwcomposer.con_svma \
+	audio.primary.con_svma \
+	memtrack.con_svma \
+	camera.con_svma
+
+PRODUCT_PACKAGES += fs_config_files
+
+# omx
+PRODUCT_PACKAGES += \
+	libstagefrighthw \
+	libnx_video_api \
+	libNX_OMX_VIDEO_DECODER \
+	libNX_OMX_VIDEO_ENCODER \
+	libNX_OMX_Base \
+	libNX_OMX_Core \
+	libNX_OMX_Common
+
+# stagefright FFMPEG compnents
+ifeq ($(EN_FFMPEG_AUDIO_DEC),true)
+PRODUCT_PACKAGES += libNX_OMX_AUDIO_DECODER_FFMPEG
+endif
+
+ifeq ($(EN_FFMPEG_EXTRACTOR),true)
+PRODUCT_PACKAGES += libNX_FFMpegExtractor
+endif
+
+DEVICE_PACKAGE_OVERLAYS := device/nexell/con_svma/overlay
+
+# increase dex2oat threads to improve booting time
+PRODUCT_PROPERTY_OVERRIDES += \
+	dalvik.vm.boot-dex2oat-threads=4 \
+	dalvik.vm.dex2oat-threads=4 \
+	dalvik.vm.image-dex2oat-threads=4
+
+#Enabling video for live effects
+-include frameworks/base/data/videos/VideoPackage1.mk
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapstartsize=16m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=8m
+
+# HWUI common settings
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
+
+#skip boot jars check
+SKIP_BOOT_JARS_CHECK := true
+
+#bootanimation
+PRODUCT_COPY_FILES += \
+	device/nexell/con_svma/bootanimation.zip:system/media/bootanimation.zip
+
+
+$(call inherit-product, frameworks/base/data/fonts/fonts.mk)
