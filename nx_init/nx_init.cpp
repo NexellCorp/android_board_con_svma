f451363a3cf07b2471292fd6322179c2887acb45
mcjino <allan.park@nexell.co.kr>
2018-11-06 13:35:41 +0900

    con_svma: add nx_init application
    This patch add the nx_init application.
    
    Signed-off-by: mcjino <allan.park@nexell.co.kr>

---
+++ nx_init/nx_init.cpp
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/mount.h>
+
+int main(int argc, char *argv[])
+{
+	pid_t pid;
+	pid_t pid2;
+
+    mount("sysfs", "/sys", "sysfs", 0, NULL);
+	pid = fork();
+	switch(pid) {
+		case -1:
+		{
+			printf("fail create child process \n");
+			return -1;
+		}
+		case 0:
+		{
+			execl("/sbin/nx_3d_avm_quick","nx_3d_avm_quick", NULL);
+		}
+		default:
+		{
+			sleep(1);
+			execl("/init","init", NULL);
+		}
+	}
+	int status;
+    waitpid(pid, &status, 0);
+	return 0;
+}
