4955bed26239635788cb15392b1635110644185a
Hyunseok Jung <hsjung@nexell.co.kr>
2019-05-09 21:31:31 +0900

    con_svm: delete unused files
    This patch delete unused files.
    it don't need audio_policy.conf and tiny_hw.con_svma.xml at android pie.
    
    Signed-off-by: Hyunseok Jung <hsjung@nexell.co.kr>
    Change-Id: I3eb2e83a26d5c39849218bc6b487deab12d382d2

--- device.mk
+++ device.mk
@@ -182,8 +182,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
-    $(LOCAL_PATH)/audio/tiny_hw.con_svma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/tiny_hw.con_svma.xml \
-    $(LOCAL_PATH)/audio/audio_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy.conf
 
 # usb
 # PRODUCT_COPY_FILES += \
