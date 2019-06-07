81de84d6d4c3f7c14ed67602ff3cd18055bffb2c
Allan park <allan.park@nexell.co.kr>
2019-06-07 16:45:46 +0900

    dtimg: add task to create dtb.img
    This patch add task to create dtb.img.
    
    Signed-off-by: Allan park <allan.park@nexell.co.kr>
    Change-Id: I3913ea674f05c69ebd4f2be45baf60daf6e6da1e
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/android_board_con_svma/+/457180

---
+++ build/tasks/dtimage.mk
@@ -0,0 +1,19 @@
+ifneq ($(filter con_svma, $(TARGET_DEVICE)),)
+ifneq ($(TARGET_NO_DTIMAGE), true)
+
+MKDTIMG := device/nexell/tools/mkdtimg
+DTB_DIR := device/nexell/kernel/kernel-4.4.x/arch/arm/boot/dts
+DTB_REV00 := $(DTB_DIR)/s5p4418-con_svma-rev00.dtb
+
+$(PRODUCT_OUT)/dtb.img: $(DTB_REV00)
+	$(MKDTIMG) create $@ \
+	$(DTB_REV00) --id=0
+
+droidcore: $(PRODUCT_OUT)/dtb.img
+
+
+# Images will be packed into target_files zip.
+INSTALLED_RADIOIMAGE_TARGET += $(PRODUCT_OUT)/dtb.img
+
+endif
+endif
