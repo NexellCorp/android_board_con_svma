0543260fe0646f0835455251f5d43c3ef904c816
Allan park <allan.park@nexell.co.kr>
2019-06-17 13:18:34 +0900

    build : add con_svma_rev01.dtb to dtimg
    This patch add con_svma_rev01.dtb to dtimg.
    
    Signed-off-by: Allan park <allan.park@nexell.co.kr>
    Change-Id: If3dfae32c3f9ddbdb8b794e55c7d4cb1f4f9616a
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/android_board_con_svma/+/458220

--- build/tasks/dtimage.mk
+++ build/tasks/dtimage.mk
@@ -4,10 +4,13 @@ ifneq ($(TARGET_NO_DTIMAGE), true)
 MKDTIMG := device/nexell/tools/mkdtimg
 DTB_DIR := device/nexell/kernel/kernel-4.4.x/arch/arm/boot/dts
 DTB_REV00 := $(DTB_DIR)/s5p4418-con_svma-rev00.dtb
+DTB_REV01 := $(DTB_DIR)/s5p4418-con_svma-rev01.dtb
 
 $(PRODUCT_OUT)/dtb.img: $(DTB_REV00)
 	$(MKDTIMG) create $@ \
-	$(DTB_REV00) --id=0
+	$(DTB_REV00) --id=0 \
+	$(DTB_REV01) --id=1
+
 
 droidcore: $(PRODUCT_OUT)/dtb.img
 
