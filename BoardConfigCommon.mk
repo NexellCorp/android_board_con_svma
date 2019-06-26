7029a40560635fc4b71ac59c2745ab0c50334f26
Lee Geunjeong <kjlee@nexell.co.kr>
2019-06-26 16:42:10 +0900

    con_svma: Add Wi-Fi features
    This patch adds Wi-Fi features.
    
    Signed-off-by: Lee Geunjeong <kjlee@nexell.co.kr>
    Change-Id: Idde47977df7c61fd30cb00fc8731fbb53692ba3c
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/android_board_con_svma/+/459410
    Reviewed-by: Sungwoo Park <swpark@nexell.co.kr>
    Tested-by: Sungwoo Park <swpark@nexell.co.kr>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,3 +109,16 @@ SND_BT_CARD_ID := 0
 SND_BT_DEVICE_ID := 0
 SND_BT_SCO_CARD_ID := 0
 SND_BT_SCO_DEVICE_ID := 1
+
+# Wifi releated defines
+WPA_SUPPLICANT_VERSION      := VER_0_8_X
+BOARD_WLAN_DEVICE           := bcmdhd
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_HOSTAPD_DRIVER        := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB   := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_AP      := "/vendor/firmware/fw_bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_STA     := "/vendor/firmware/fw_bcmdhd.bin"
+WIFI_DRIVER_FW_PATH_P2P     := ""
+WIFI_HIDL_FEATURE_DUAL_INTERFACE := true
