939ffd98cfdf227f8b03d026395fb306da145fbb
Lee Geunjeong <kjlee@nexell.co.kr>
2019-07-26 14:51:43 +0900

    con_svma: Update BT/WiFi features
    This patch separates the BT/WiFi config into s5p4418 and s5p6818.
    This patch suppors BT LPM.
    
    Signed-off-by: Lee Geunjeong <kjlee@nexell.co.kr>
    Change-Id: I40a3b0033fc446b81037107772ecbc9f4d6fc14d

--- soc_s5p4418.mk
+++ soc_s5p4418.mk
@@ -7,3 +7,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/nexell/con_svma/init.$(PRODUCT_HARDWARE).rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_HARDWARE).rc \
     device/nexell/con_svma/fstab.$(PRODUCT_HARDWARE):$(TARGET_COPY_OUT_VENDOR)/etc/fstab.$(PRODUCT_HARDWARE)
+
+# bluetooth
+PRODUCT_COPY_FILES += \
+    device/nexell/con_svma/bluetooth/bt_vendor_s5p4418.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_vendor.conf
--- soc_s5p6818.mk
+++ soc_s5p6818.mk
@@ -2,3 +2,7 @@
 PRODUCT_COPY_FILES += \
     device/nexell/con_svma/init.$(PRODUCT_HARDWARE).s5p6818.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_HARDWARE).rc \
     device/nexell/con_svma/fstab.s5p6818:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.$(PRODUCT_HARDWARE)
+
+# bluetooth
+PRODUCT_COPY_FILES += \
+    device/nexell/con_svma/bluetooth/bt_vendor_s5p6818.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_vendor.conf
