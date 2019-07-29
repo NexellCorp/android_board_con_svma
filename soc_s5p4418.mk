958e75f87b8dcab32117d596a4bd426df935ebe6
Allan park <allan.park@nexell.co.kr>
2019-07-29 16:19:05 +0900

    s5p6818_con_svma: remove the kernel and dtb copy action
    The kernel and dtb files are copied on build.sh, therefore, it
    does not need to add to PRODUCT_COPY_FILES.
    
    Signed-off-by: Allan park <allan.park@nexell.co.kr>
    Change-Id: Ic74bd202981fe7a15233fc326495f91b69810bdc
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/android_board_con_svma/+/463472

--- soc_s5p4418.mk
+++ soc_s5p4418.mk
@@ -1,8 +1,4 @@
 # kernel
-PRODUCT_COPY_FILES += \
-    device/nexell/kernel/kernel-4.4.x/arch/arm/boot/zImage:kernel \
-    device/nexell/kernel/kernel-4.4.x/arch/arm/boot/dts/s5p4418-con_svma-rev00.dtb:2ndbootloader
-
 # fstab
 PRODUCT_COPY_FILES += \
     device/nexell/con_svma/init.$(PRODUCT_HARDWARE).rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_HARDWARE).rc \
