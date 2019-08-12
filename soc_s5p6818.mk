5dc12df4b9b92946243660dde29839f166d4ed95
Sungwoo Park <swpark@nexell.co.kr>
2019-08-12 13:47:48 +0900

    quickboot/s5p6818: Overrides init.con_svma.rc
    When QUICKBOOT is enabled, this patch overrides init.con_svma.rc
    by init.con_svma.s5p6818.quickboot.rc file.
    
    Following is major difference.
    - Remove "on-fs" entry
    - late-start is triggered when boot is completed
    
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>
    Change-Id: I482719b000f66cc3d5b880dc4591e2d6d6608e4b
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/android_board_con_svma/+/464829

--- soc_s5p6818.mk
+++ soc_s5p6818.mk
@@ -1,6 +1,14 @@
+# init.{PRODUCT_HARDWARE}.rc
+ifeq ($(QUICKBOOT), 1)
+PRODUCT_COPY_FILES += \
+    device/nexell/con_svma/init.$(PRODUCT_HARDWARE).s5p6818.quickboot.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_HARDWARE).rc
+else
+PRODUCT_COPY_FILES += \
+    device/nexell/con_svma/init.$(PRODUCT_HARDWARE).s5p6818.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_HARDWARE).rc
+endif
+
 # fstab
 PRODUCT_COPY_FILES += \
-    device/nexell/con_svma/init.$(PRODUCT_HARDWARE).s5p6818.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_HARDWARE).rc \
     device/nexell/con_svma/fstab.s5p6818:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.$(PRODUCT_HARDWARE)
 
 # bluetooth
