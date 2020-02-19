a0a9e1151d7bcdd54ec70f9e21ee0eb357738b20
allan park <allan.park@nexell.co.kr>
2020-02-19 16:12:23 +0900

    s5p4418_con_svma: modify build mk directory
    This patch modify build mk directory.
    
    Signed-off-by: allan park <allan.park@nexell.co.kr>
    Change-Id: I83f97b88336fc58deaf43178d1d3984608d198f3
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/android_board_con_svma/+/485201

--- s5p4418_con_svma/AndroidBoard.mk
+++ s5p4418_con_svma/AndroidBoard.mk
@@ -2,12 +2,12 @@ LOCAL_PATH := $(call my-dir)
 
 $(info "======= AndroidBoard.mk enter =========")
 
-include vendor/nexell/common/kernel.mk
-include vendor/nexell/common/dtbo.mk
-include vendor/nexell/common/bl1_s5p4418.mk
-include vendor/nexell/common/bl2.mk
-include vendor/nexell/common/armv7-dispacher.mk
-include vendor/nexell/common/uboot.mk
+include vendor/nexell/build/kernel.mk
+include vendor/nexell/build/dtbo.mk
+include vendor/nexell/build/bl1_s5p4418.mk
+include vendor/nexell/build/bl2.mk
+include vendor/nexell/build/armv7-dispacher.mk
+include vendor/nexell/build/uboot.mk
 
 $(info "======= AndroidBoard.mk exit ==========")
 
