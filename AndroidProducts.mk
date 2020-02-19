ed026ddc734a4c39b724900646d368bd046b27e6
allan park <allan.park@nexell.co.kr>
2020-02-19 14:14:50 +0900

    con_svma: change to structure to apply android build system
    This patch change to structure to apply android build system.
    
    Signed-off-by: allan park <allan.park@nexell.co.kr>
    Change-Id: I66345249b12ee8f5c018090393406d05549348dd
    Reviewed-on: https://review.gerrithub.io/c/NexellCorp/android_board_con_svma/+/485193

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,7 @@
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/con_svma.mk \
-    $(LOCAL_DIR)/con_svma_auto.mk
+    s5p4418_con_svma:$(LOCAL_DIR)/s5p4418_con_svma/con_svma.mk \
+    s5p4418_con_svma_auto:$(LOCAL_DIR)/s5p4418_con_svma/con_svma_auto.mk \
+    s5p6818_con_svma:$(LOCAL_DIR)/s5p6818_con_svma/con_svma.mk \
+    s5p6818_con_svma_auto:$(LOCAL_DIR)/s5p6818_con_svma/con_svma_auto.mk
 
-COMMON_LUNCH_CHOICES := \
-    con_svma-eng \
-    con_svma-user \
-    con_svma-userdebug \
-    con_svma_auto-eng \
-    con_svma_auto-user \
-    con_svma_auto-userdebug
---
+++ s5p4418_con_svma/con_svma.mk
@@ -0,0 +1,18 @@
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_no_telephony.mk)
+
+PRODUCT_NAME := s5p4418_con_svma
+PRODUCT_DEVICE := s5p4418_con_svma
+PRODUCT_BRAND := con_svma
+PRODUCT_MODEL := con_svma
+PRODUCT_MANUFACTURER := Nexell
+PRODUCT_HARDWARE := con_svma
+
+# automatically called
+-include device/nexell/con_svma/common/device_common.mk
+
+DEVICE_MANIFEST_FILE := device/nexell/con_svma/common/manifest.xml
+
+-include device/nexell/con_svma/s5p4418_con_svma/common.mk
+
