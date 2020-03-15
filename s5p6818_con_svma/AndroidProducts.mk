a72a9f283f69c921ce21e950e913869b1bfbd259
allan park <allan.park@nexell.co.kr>
2020-03-15 15:51:54 +0900

    con_svma: s5p6818 fix the AndroidProducts path
    This patch fix the AndroidProducts path.
    
    Signed-off-by: allan park <allan.park@nexell.co.kr>
    Change-Id: Ic3efef7ac556219e16b56ef68e3735edddd7a52b

--- s5p6818_con_svma/AndroidProducts.mk
+++ s5p6818_con_svma/AndroidProducts.mk
@@ -1,5 +1,5 @@
 
 PRODUCT_MAKEFILES := \
-    s5p6818_con_svma:$(LOCAL_DIR)/s5p6818_con_svma/con_svma.mk \
-    s5p6818_con_svma_auto:$(LOCAL_DIR)/s5p6818_con_svma/con_svma_auto.mk
+    s5p6818_con_svma:$(LOCAL_DIR)/con_svma.mk \
+    s5p6818_con_svma_auto:$(LOCAL_DIR)/con_svma_auto.mk
 
