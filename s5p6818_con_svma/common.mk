7521e504996acf32aa3731c3b22083bf1d0e7d4e
allan park <allan.park@nexell.co.kr>
2020-03-15 14:43:20 +0900

    con_svma: remove common and seperate to nxp4330 and s5p6818
    This patch remove common and seperate to nxp4330 and s5p6818.
    
    Signed-off-by: allan park <allan.park@nexell.co.kr>
    Change-Id: Icc4ba202d58ef6acca2ccecd5530cdeecebb5b79

--- s5p6818_con_svma/common.mk
+++ s5p6818_con_svma/common.mk
@@ -8,13 +8,13 @@ endif
 
 # fstab
 PRODUCT_COPY_FILES += \
-    device/nexell/con_svma/common/fstab.con_svma:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.$(PRODUCT_HARDWARE)
+    device/nexell/con_svma/s5p6818_con_svma/fstab.con_svma:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.$(PRODUCT_HARDWARE)
 
 # bluetooth
 PRODUCT_COPY_FILES += \
     device/nexell/con_svma/s5p6818_con_svma/bluetooth_config/bt_vendor_s5p6818.conf:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth/bt_vendor.conf
 
 #recovery
-TARGET_RECOVERY_FSTAB := device/nexell/con_svma/common/fstab.con_svma
+TARGET_RECOVERY_FSTAB := device/nexell/con_svma/s5p6818_con_svma/fstab.con_svma
 PRODUCT_COPY_FILES += \
     device/nexell/con_svma/s5p6818_con_svma/init.recovery.con_svma.rc:root/init.recovery.$(PRODUCT_HARDWARE).rc
